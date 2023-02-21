326b3f9be5c1cfeb0370d88b4a15c84c8124d67c
MaggH-Jones <123364051+MaggH-N-Jones@users.noreply.github.com>
2023-02-21 15:09:34 +0100

    Add JavaScript

--- webapp/index.js
+++ webapp/index.js
@@ -1,8 +1,21 @@
 let loginButton = document.getElementById("loginButton");
+let signupButton = document.getElementById("signupButton");
 
+/**
+ * 
+ * @param {string} pagePath 
+ */
 
+function redirectToPage(pagePath) {
+    window.location.pathname = pagePath
+}
 
 loginButton.addEventListener("click", () => {
-    console.log('BOO');
+    redirectToPage("/login.html")
 
 });
+
+let click = "click"
+let alfred = () => { redirectToPage("/register.html") }
+
+signupButton.addEventListener(click, alfred);
