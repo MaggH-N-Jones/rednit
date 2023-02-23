8f18eabfa11099d60df3a04678aa474f310ba884
MaggH-Jones <123364051+MaggH-N-Jones@users.noreply.github.com>
2023-02-23 12:11:12 +0100

    add loginPage

--- webapp/login.js
+++ webapp/login.js
@@ -1,11 +1,33 @@
 let usernameInput = document.getElementById("username");
 let passwordInput = document.getElementById("password");
-let loginCommitButton = document.getElementById("login-commit-button");
+let loginButton = document.getElementById("login-button");
+function redirectToPage(pagePath) {
+    window.location.pathname = pagePath
+}
 
-
-
-
-
-loginCommitButton.addEventListener("click", () => {
-    console.log(usernameInput.value);
+loginButton.addEventListener("click", async () => {
+    let username = usernameInput.value;
+    let password = passwordInput.value;
+    let response = await sendLoginToBackend(username, password);
+    if (response.ok) {
+        sessionStorage.setItem("token", response.token)
+        redirectToPage("/swiper.html")
+    }
+    else {
+        alert(`Failed to login: ${response.errorMessage}`);
+    }
 })
+
+async function sendLoginToBackend(username, password) {
+    let request = {
+        username: username,
+        password: password,
+    }
+    let response = await fetch("/api/users/login", {
+        method: "POST",
+        headers: new Headers({ "Content-Type": "application/json" }),
+        body: JSON.stringify(request)
+    });
+    let responseData = await response.json();
+    return responseData;
+}
