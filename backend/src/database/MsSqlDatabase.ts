931b4d41ae176e0e2cea88d61a87f14694d8c3ff
MaggH-Jones <123364051+MaggH-N-Jones@users.noreply.github.com>
2023-03-02 14:19:03 +0100

    add mssssql database

---
+++ backend/src/database/MsSqlDatabase.ts
@@ -0,0 +1,64 @@
+import { User } from "../users/User";
+import { Database } from "./Database"
+import mssql, { ConnectionPool } from "mssql";
+
+export class MsSqlDatabase implements Database {
+
+    private connection: ConnectionPool;
+    private userIdCounter = 0;
+
+    constructor() {
+        var config = {
+            user: 'sa',
+            password: 'mypassword',
+            server: 'localhost', 
+            Database: 'SchoolDB' 
+        };
+        
+        const newConnection = new ConnectionPool(config);
+        this.connection = newConnection;
+    }
+
+    async doesUserWithUsernameExist(username: string): Promise<boolean> {
+        const request = new mssql.Request(this.connection);
+        request.input("username", username)
+        try {
+            const users = await request.query("SELECT * FROM users WHERE username=@username;")
+            return users.recordset.length > 0;
+        } catch {
+            return true;
+        }
+    }
+    async addUser(user: User): Promise<void> {
+        const request = new mssql.Request(this.connection);
+        request.input("id", user.id)
+        request.input("username", user.username)
+        request.input("password", user.password)
+        request.input("name", user.name)
+        request.input("age", user.age)
+        try {
+            await request.query(`
+                INSERT INTO table_name (id, username, password, name, age) 
+                VALUES (@id, @username, @password, @name, @age);
+            `)
+        } catch {
+        }
+    }
+    async uniqueUserId(): Promise<number> {
+        const id = this.userIdCounter;
+        this.userIdCounter += 1;
+        return id;
+    }
+    async isUsersPasswordCorrect(username: string, password: string): Promise<boolean> {
+        const request = new mssql.Request(this.connection);
+        request.input("username", username)
+        request.input("password", password)
+        try {
+            const users = await request.query("SELECT * FROM users WHERE username=@username AND password=@password;")
+            return users.recordset.length > 0;
+        } catch {
+            return true;
+        }
+    }
+    
+}
